1df309e09d4a2bb994ccd860e86fc70f1486236b
linsicai <linsicai@gmail.com>
2019-03-21 16:47:02 +0800

    add comment
    add comment

--- pkg/wait/wait.go
+++ pkg/wait/wait.go
@@ -17,75 +17,104 @@
 package wait
 
 import (
-	"log"
-	"sync"
+    "log"
+    "sync"
 )
 
 // Wait is an interface that provides the ability to wait and trigger events that
 // are associated with IDs.
 type Wait interface {
-	// Register waits returns a chan that waits on the given ID.
-	// The chan will be triggered when Trigger is called with
-	// the same ID.
-	Register(id uint64) <-chan interface{}
-	// Trigger triggers the waiting chans with the given ID.
-	Trigger(id uint64, x interface{})
-	IsRegistered(id uint64) bool
+    // Register waits returns a chan that waits on the given ID.
+    // The chan will be triggered when Trigger is called with
+    // the same ID.
+    Register(id uint64) <-chan interface{}
+
+    // Trigger triggers the waiting chans with the given ID.
+    // 触发
+    Trigger(id uint64, x interface{})
+
+    // 判断是否注册
+    IsRegistered(id uint64) bool
 }
 
+// 列表
 type list struct {
-	l sync.RWMutex
-	m map[uint64]chan interface{}
+    // 读写锁
+    l sync.RWMutex
+
+    // 映射表
+    m map[uint64]chan interface{}
 }
 
 // New creates a Wait.
+// 创建列表
 func New() Wait {
-	return &list{m: make(map[uint64]chan interface{})}
+    return &list{
+        m: make(map[uint64]chan interface{})
+    }
 }
 
+// 注册
 func (w *list) Register(id uint64) <-chan interface{} {
-	w.l.Lock()
-	defer w.l.Unlock()
-	ch := w.m[id]
-	if ch == nil {
-		ch = make(chan interface{}, 1)
-		w.m[id] = ch
-	} else {
-		log.Panicf("dup id %x", id)
-	}
-	return ch
+    // 锁
+    w.l.Lock()
+    defer w.l.Unlock()
+
+    ch := w.m[id]
+    if ch == nil {
+        // 新增ID
+        ch = make(chan interface{}, 1)
+        w.m[id] = ch
+    } else {
+        // 重复ID 异常
+        log.Panicf("dup id %x", id)
+    }
+
+    return ch
 }
 
+// 触发
 func (w *list) Trigger(id uint64, x interface{}) {
-	w.l.Lock()
-	ch := w.m[id]
-	delete(w.m, id)
-	w.l.Unlock()
-	if ch != nil {
-		ch <- x
-		close(ch)
-	}
+    w.l.Lock()
+
+    // 删除ID
+    ch := w.m[id]
+    delete(w.m, id)
+
+    w.l.Unlock()
+
+    if ch != nil {
+        // 触发，并投放一个接口
+        ch <- x
+
+        close(ch)
+    }
 }
 
+// 判断是否注册
 func (w *list) IsRegistered(id uint64) bool {
-	w.l.RLock()
-	defer w.l.RUnlock()
-	_, ok := w.m[id]
-	return ok
+    w.l.RLock()
+    defer w.l.RUnlock()
+
+    _, ok := w.m[id]
+    return ok
 }
 
 type waitWithResponse struct {
-	ch <-chan interface{}
+    // 一个通道
+    ch <-chan interface{}
 }
-
 func NewWithResponse(ch <-chan interface{}) Wait {
-	return &waitWithResponse{ch: ch}
+    return &waitWithResponse{
+        ch: ch
+    }
 }
-
 func (w *waitWithResponse) Register(id uint64) <-chan interface{} {
-	return w.ch
+    return w.ch
 }
-func (w *waitWithResponse) Trigger(id uint64, x interface{}) {}
+func (w *waitWithResponse) Trigger(id uint64, x interface{}) {
+}
 func (w *waitWithResponse) IsRegistered(id uint64) bool {
-	panic("waitWithResponse.IsRegistered() shouldn't be called")
+    // 抛出（运行时）异常
+    panic("waitWithResponse.IsRegistered() shouldn't be called")
 }
--- pkg/wait/wait_test.go
+++ pkg/wait/wait_test.go
@@ -20,59 +20,78 @@ import (
 	"time"
 )
 
+// 基础测试
 func TestWait(t *testing.T) {
 	const eid = 1
+
+	// 注册 & 触发
 	wt := New()
 	ch := wt.Register(eid)
 	wt.Trigger(eid, "foo")
+
+	// 预期读到
 	v := <-ch
 	if g, w := fmt.Sprintf("%v (%T)", v, v), "foo (string)"; g != w {
 		t.Errorf("<-ch = %v, want %v", g, w)
 	}
 
+	// 预期不可读取
 	if g := <-ch; g != nil {
 		t.Errorf("unexpected non-nil value: %v (%T)", g, g)
 	}
 }
 
+// 测试重新ID
 func TestRegisterDupPanic(t *testing.T) {
 	const eid = 1
+
+	// 注册
 	wt := New()
 	ch1 := wt.Register(eid)
 
 	panicC := make(chan struct{}, 1)
 
+	// 匿名函数且执行
 	func() {
+		// 函数执行完后执行
 		defer func() {
+			// 如果有异常，记录下来
 			if r := recover(); r != nil {
 				panicC <- struct{}{}
 			}
 		}()
+
 		wt.Register(eid)
 	}()
 
 	select {
-	case <-panicC:
+	case <-panicC:	// 此处应该有异常
 	case <-time.After(1 * time.Second):
+		// 不应该到这里
 		t.Errorf("failed to receive panic")
 	}
 
+	// 触发且收数据
 	wt.Trigger(eid, "foo")
 	<-ch1
 }
 
 func TestTriggerDupSuppression(t *testing.T) {
 	const eid = 1
+
+	// 注册与触发
 	wt := New()
 	ch := wt.Register(eid)
 	wt.Trigger(eid, "foo")
 	wt.Trigger(eid, "bar")
 
+	// 测试可读取
 	v := <-ch
 	if g, w := fmt.Sprintf("%v (%T)", v, v), "foo (string)"; g != w {
 		t.Errorf("<-ch = %v, want %v", g, w)
 	}
 
+	// 测试不可读取
 	if g := <-ch; g != nil {
 		t.Errorf("unexpected non-nil value: %v (%T)", g, g)
 	}
@@ -81,20 +100,24 @@ func TestTriggerDupSuppression(t *testing.T) {
 func TestIsRegistered(t *testing.T) {
 	wt := New()
 
+	// 注册
 	wt.Register(0)
 	wt.Register(1)
 	wt.Register(2)
 
+	// 删除已注册
 	for i := uint64(0); i < 3; i++ {
 		if !wt.IsRegistered(i) {
 			t.Errorf("event ID %d isn't registered", i)
 		}
 	}
 
+	// 测试未注册
 	if wt.IsRegistered(4) {
 		t.Errorf("event ID 4 shouldn't be registered")
 	}
 
+	// 测试触发后的
 	wt.Trigger(0, "foo")
 	if wt.IsRegistered(0) {
 		t.Errorf("event ID 0 is already triggered, shouldn't be registered")

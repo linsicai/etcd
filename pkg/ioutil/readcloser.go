1f7ba35e1458192b2f3cd2b6f63877f9112c75c4
linsicai <linsicai@gmail.com>
2019-03-25 14:31:06 +0800

    fini ioutil
    fini ioutil

--- pkg/ioutil/readcloser.go
+++ pkg/ioutil/readcloser.go
@@ -15,62 +15,62 @@
 package ioutil
 
 import (
-	"fmt"
-	"io"
+    "fmt"
+    "io"
 )
 
 // ReaderAndCloser implements io.ReadCloser interface by combining
 // reader and closer together.
 type ReaderAndCloser struct {
-	io.Reader
-	io.Closer
+    io.Reader
+    io.Closer
 }
 
 var (
-	ErrShortRead = fmt.Errorf("ioutil: short read")
-	ErrExpectEOF = fmt.Errorf("ioutil: expect EOF")
+    ErrShortRead = fmt.Errorf("ioutil: short read")
+    ErrExpectEOF = fmt.Errorf("ioutil: expect EOF")
 )
 
 // NewExactReadCloser returns a ReadCloser that returns errors if the underlying
 // reader does not read back exactly the requested number of bytes.
 func NewExactReadCloser(rc io.ReadCloser, totalBytes int64) io.ReadCloser {
-	return &exactReadCloser{rc: rc, totalBytes: totalBytes}
+    return &exactReadCloser{rc: rc, totalBytes: totalBytes}
 }
 
 type exactReadCloser struct {
-	rc         io.ReadCloser
+    rc         io.ReadCloser
 
-	br         int64
-	totalBytes int64
+    br         int64
+    totalBytes int64
 }
 
 func (e *exactReadCloser) Read(p []byte) (int, error) {
-	n, err := e.rc.Read(p)
+    n, err := e.rc.Read(p)
 
     // 读多了
-	e.br += int64(n)
-	if e.br > e.totalBytes {
-		return 0, ErrExpectEOF
-	}
+    e.br += int64(n)
+    if e.br > e.totalBytes {
+        return 0, ErrExpectEOF
+    }
 
     // 读少了
-	if e.br < e.totalBytes && n == 0 {
-		return 0, ErrShortRead
-	}
+    if e.br < e.totalBytes && n == 0 {
+        return 0, ErrShortRead
+    }
 
-	return n, err
+    return n, err
 }
 
 func (e *exactReadCloser) Close() error {
     // 关闭
-	if err := e.rc.Close(); err != nil {
-		return err
-	}
+    if err := e.rc.Close(); err != nil {
+        return err
+    }
 
     // 未读完
-	if e.br < e.totalBytes {
-		return ErrShortRead
-	}
+    if e.br < e.totalBytes {
+        return ErrShortRead
+    }
 
-	return nil
+    return nil
 }
